e46271290633a83ea0cac13266c64eb40c87c92c
Felix <fxvargas7@outlook.com>
2023-08-18 00:40:53 -0400

    Initial commit

---
+++ e-commerce-site-w-react-vite/src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import {Button, Container, Nav, Navbar as NavbarBs} from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+    return (
+        <NavbarBs sticky="top" className={"bg-white shadow-sm mb-3"}>
+          <Container>
+            <Nav className="me-auto">
+                <Nav.Link to="/" as={NavLink}>
+                    HOME
+                </Nav.Link>
+                <Nav.Link to="/store" as={NavLink}>
+                    STORE
+                </Nav.Link>
+                <Nav.Link to="/about" as={NavLink}>
+                    ABOUT
+                </Nav.Link>
+            </Nav>
+              <Button
+                  style={{ width: "3rem", height: "3rem", position: "relative" }}
+                  variant="outline-primary"
+                  className="rounded-circle">
+                  <img src="../assets/image/shopping-cart-outline-svgrepo-com.svg" alt="shopping-cart-icon" />
+                  <div  className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                        style={{ color: "white",
+                            width: "1.5rem",
+                            height: "1.5rem",
+                            position: "absolute",
+                            bottom: 0,
+                            right: 0,
+                            transform: "translate(25%, 25%)"}}>
+                      7
+                  </div>
+              </Button>
+          </Container>
+        </NavbarBs>
+    )
+}
